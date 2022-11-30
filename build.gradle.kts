df26e588f6e7ffdea8360b261158f562bed8c7a0
Aditya Gupta <52538173+aadityaguptaa@users.noreply.github.com>
2022-11-30 22:28:06 +0530

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    kotlin("jvm") version "1.7.22"
+}
+
+repositories {
+    mavenCentral()
+}
+
+tasks {
+    sourceSets {
+        main {
+            java.srcDirs("src")
+        }
+    }
+
+    wrapper {
+        gradleVersion = "7.6"
+    }
+}
---
+++ src/Utils.kt
@@ -0,0 +1,16 @@
+import java.io.File
+import java.math.BigInteger
+import java.security.MessageDigest
+
+/**
+ * Reads lines from the given input txt file.
+ */
+fun readInput(name: String) = File("src", "$name.txt")
+    .readLines()
+
+/**
+ * Converts string to md5 hash.
+ */
+fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
+    .toString(16)
+    .padStart(32, '0')
